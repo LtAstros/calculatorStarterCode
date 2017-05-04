d85e39bea35de2f4b4b5ce9d518bd09d62568425
LtAstros <costinsf11@gmail.com>
2017-05-04 16:38:23 -0700

    Update guts.js

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -3,35 +3,64 @@
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
 
 
+// 1. divide
+//    write a function named "divide" that takes in two parameters and returns their quotient
 function divide(x,y) {
-	//Write your code below this line
-
+	return x / y;
+}
+console.log(divide(1,2));
+// 2. multiply
+// write a function named
+// "multiply" that takes in two
+// parameters and returns
+// their product
+function multiply(x,y){
+ return x * y;   
+}
+console.log(multiply(1,2));
+// 3. subtract 
+//    write a function named
+//    "subtract" that takes in two
+// parameters and returns
+// their difference
+function subtract(x,y){
+ return x - y;   
+}
+console.log(subtract(1,2));
+// 4. add
+//    write a function named "add" that takes in two parameters and returns their sum
+function add(x,y){
+ return x + y;   
+}
+console.log(add(1,2));
+// 5. power
+//    write a function named "power" that takes in two parameters and returns their result
+function power(x,y){
+ return Math.pow(x,y);   
+}
+console.log(power(3,2));
+// 6. square_root
+//    write a function named "square_root" that takes in one parameter and returns their result
+function square_root(x){
+ return Math.sqrt(x);   
+}
+console.log(square_root(4));
+// 7. logBase10
+//    write a function named "logBase10" that takes in one parameter and returns their result
+function logBase10(x){
+ return Math.log10(x);   
+}
+console.log(logBase10(100));
+// 8. generateRandomNumber
+//    write a function named "generateRandomNumber" that takes in no parameters and returns a random number
+function generateRandomNumber(){
+ return Math.random();   
 }
 
-function multiply(x,y) {
-
-}
-
-function subtract(x,y) {
-
-}
-
-function add(x,y) {
-
-}
-
-function power(x,y) {
-
-}
-
-function square_root(x) {
-
-}
-
-function logBase10(x) {
-
-}
-
-function generateRandomNumber() {
-
-}
+console.log(generateRandomNumber());
+// BONUS: write assertions to test whether your functions work!
+// Here are some examples for divide(x,y), can you write more for the other functions?
+//
+// console.assert(divide(1,1) === 1, "1/1 should equal 1");
+// console.assert(divide(1,2) === 0.5, "1/2 should equal 0.5");
+console.assert(logBase10(1000) === 3);
